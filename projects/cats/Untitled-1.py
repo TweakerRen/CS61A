074113066f7f0dd9eb94d27d66ebd61a750b5e36
TweakerRen <cyigy3@163.com>
2025-07-19 21:50:06 +0800

    try3

---
+++ projects/cats/Untitled-1.py
@@ -0,0 +1,155 @@
+def max_product(s):
+    """Return the maximum product of non-consecutive elements of s.
+
+    >>> max_product([10, 3, 1, 9, 2])   # 10 * 9
+    90
+    >>> max_product([5, 10, 5, 10, 5])  # 5 * 5 * 5
+    125
+    >>> max_product([])                 # The product of no numbers is 1
+    1
+    """
+    if not s:
+        return 1
+    else:
+        return max(s[0] * max_product(s[2:]), max_product(s[1:]))
+
+def sums(n, m):
+    """Return lists that sum to n containing positive numbers up to m that
+    have no adjacent repeats.
+
+    >>> sums(5, 1)
+    []
+    >>> sums(5, 2)
+    [[2, 1, 2]]
+    >>> sums(5, 3)
+    [[1, 3, 1], [2, 1, 2], [2, 3], [3, 2]]
+    >>> sums(5, 5)
+    [[1, 3, 1], [1, 4], [2, 1, 2], [2, 3], [3, 2], [4, 1], [5]]
+    >>> sums(6, 3)
+    [[1, 2, 1, 2], [1, 2, 3], [1, 3, 2], [2, 1, 2, 1], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+    """
+    if n < 0:
+        return []
+    if n == 0:
+        sums_to_zero = []     # The only way to sum to zero using positives
+        return [sums_to_zero] # Return a list of all the ways to sum to zero
+    result = []
+    for k in range(1, m + 1):
+        result = result + [ [k] + rest for rest in sums(n-k, m) if rest == [] or k != rest[0] ]
+    return result
+
+
+def feline_fixes(typed, source, limit):
+    if limit == 0:
+        return limit+1
+    if typed[1:] == "" or source[1:] == "":
+        return len(max(typed[1:], source[1:]))
+    if typed[0] != source[0]:
+        return 1 + feline_fixes(typed[1:], source[1:], limit-1)
+    else:
+        return feline_fixes(typed[1:], source[1:], limit-1)
+    # END PROBLEM 6
+def fastest_words(match):
+    """Return a list of lists of which words each player typed fastest.
+
+    Arguments:
+        match: a match data abstraction as returned by time_per_word.
+
+    >>> p0 = [5, 1, 3]
+    >>> p1 = [4, 1, 6]
+    >>> fastest_words(match(['Just', 'have', 'fun'], [p0, p1]))
+    [['have', 'fun'], ['Just']]
+    >>> p0  # input lists should not be mutated
+    [5, 1, 3]
+    >>> p1
+    [4, 1, 6]
+    """
+    player_indices = range(len(get_all_times(match)))  # contains an *index* for each player
+    word_indices = range(len(get_all_words(match)))    # contains an *index* for each word
+    # BEGIN PROBLEM 10
+    
+
+    outer_list = []
+    lookup_dict = {player: [] for player in player_indices}
+    for word in word_indices:
+        least_time = 999
+        least_word = ""
+
+        for player in player_indices:
+            time_spent = time(match, player, word)
+            if least_time > time_spent:
+                least_time = time_spent
+                least_word = get_word(match, word)
+                current_player = player
+
+        lookup_dict[current_player].append(least_word)
+        least_word = ""
+
+    for player in player_indices:
+        outer_list.append(lookup_dict[player])
+
+    return outer_list
+    
+    # END PROBLEM 10
+
+
+def match(words, times):
+    """A data abstraction containing all words typed and their times.
+
+    Arguments:
+        words: A list of strings, each string representing a word typed.
+        times: A list of lists for how long it took for each player to type
+            each word.
+            times[i][j] = time it took for player i to type words[j].
+
+    Example input:
+        words: ['Hello', 'world']
+        times: [[5, 1], [4, 2]]
+    """
+    assert all([type(w) == str for w in words]), 'words should be a list of strings'
+    assert all([type(t) == list for t in times]), 'times should be a list of lists'
+    assert all([isinstance(i, (int, float)) for t in times for i in t]), 'times lists should contain numbers'
+    assert all([len(t) == len(words) for t in times]), 'There should be one word per time.'
+    return {"words": words, "times": times}
+
+
+def get_word(match, word_index):
+    """A utility function that gets the word with index word_index"""
+    assert 0 <= word_index < len(get_all_words(match)), "word_index out of range of words"
+    return get_all_words(match)[word_index]
+
+
+def time(match, player_num, word_index):
+    """A utility function for the time it took player_num to type the word at word_index"""
+    assert word_index < len(get_all_words(match)), "word_index out of range of words"
+    assert player_num < len(get_all_times(match)), "player_num out of range of players"
+    return get_all_times(match)[player_num][word_index]
+
+def get_all_words(match):
+    """A selector function for all the words in the match"""
+    return match["words"]
+
+def get_all_times(match):
+    """A selector function for all typing times for all players"""
+    return match["times"]
+
+
+def match_string(match):
+    """A helper function that takes in a match data abstraction and returns a string representation of it"""
+    return f"match({get_all_words(match)}, {get_all_times(match)})"
+
+enable_multiplayer = False  # Change to True when you're ready to race.
+
+##########################
+# Command Line Interface #
+##########################
+def main():
+
+    p0 = [2, 2, 3]
+    p1 = [6, 1, 2]
+    print(fastest_words(match(['What', 'great', 'luck'], [p0, p1])))
+
+
+if __name__ == "__main__":
+    main()
+
