d3cd85fc3bb0f1078a84f9ec46bb6baa9a3be626
TweakerRen <cyigy3@163.com>
2025-07-21 00:50:02 +0800

    Jul 21 2025

--- lab/lab04/lab04.py
+++ lab/lab04/lab04.py
@@ -7,7 +7,9 @@ def divide(quotients, divisors):
     >>> divide(range(1, 5), range(20, 25))
     {1: [20, 21, 22, 23, 24], 2: [20, 22, 24], 3: [21, 24], 4: [20, 24]}
     """
-    return {____: ____ for ____ in ____}
+    return {quotient: [divisor for divisor in divisors 
+                       if divisor / quotient == divisor // quotient] 
+            for quotient in quotients }
 
 
 def buy(required_fruits, prices, total_amount):
@@ -29,9 +31,9 @@ def buy(required_fruits, prices, total_amount):
             print(cart)
         elif fruits and amount > 0:
             fruit = fruits[0]
-            price = ____
-            for k in ____:
-                add(____, ____, ____)
+            price = prices[fruit]
+            for k in range(1, amount // price + 1):
+                add(fruits[1:], amount-price * k, cart + display(fruit, k))
     add(required_fruits, total_amount, '')
 
 
@@ -63,7 +65,7 @@ def distance(city_a, city_b):
     >>> distance(city_c, city_d)
     5.0
     """
-    "*** YOUR CODE HERE ***"
+    return sqrt((get_lat(city_a) - get_lat(city_b))**2 + ((get_lon(city_a) - get_lon(city_b))**2))
 
 def closer_city(lat, lon, city_a, city_b):
     """
@@ -80,7 +82,8 @@ def closer_city(lat, lon, city_a, city_b):
     >>> closer_city(41.29, 174.78, bucharest, vienna)
     'Bucharest'
     """
-    "*** YOUR CODE HERE ***"
+    objective = make_city ('object', lat, lon)
+    return get_name(city_a) if distance(objective, city_a) < distance(objective, city_b) else get_name(city_b)
 
 def check_city_abstraction():
     """
