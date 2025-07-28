e447fff8e7452b75f4eb824e9a15f9ba2b8ca56a
TweakerRen <cyigy3@163.com>
2025-07-29 06:55:03 +0800

    Jul 29 2025

--- discussion/discussion05.py
+++ discussion/discussion05.py
@@ -1,30 +1,154 @@
-def partition_gen(n, m):
-    """Yield the partitions of n using parts up to size m.
 
-    >>> for partition in sorted(partition_gen(6, 4)):
-    ...     print(partition)
-    1 + 1 + 1 + 1 + 1 + 1
-    1 + 1 + 1 + 1 + 2
-    1 + 1 + 1 + 3
-    1 + 1 + 2 + 2
-    1 + 1 + 4
-    1 + 2 + 3
-    2 + 2 + 2
-    2 + 4
-    3 + 3
+def draw(hand, positions):
+    """Remove and return the items at positions from hand.
+
+    >>> hand = ['A', 'K', 'Q', 'J', 10, 9]
+    >>> draw(hand, [2, 1, 4])
+    ['K', 'Q', 10]
+    >>> hand
+    ['A', 'J', 9]
     """
-    assert n > 0 and m > 0
-    if n == m:
-        yield str(n)
-    if n - m > 0:
-        for p in partition_gen(n-m, m):
-            yield p + " + " + str(m)
-    if m > 1:
-        yield from partition_gen(n, m-1)
-        
+    return list(reversed([hand.pop(i) for i in reversed(sorted(positions))] ))
+
+LOWERCASE_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+
+class CapsLock:
+    def __init__(self):
+        self.pressed = 0
+
+    def press(self):
+        self.pressed += 1
+
+class Button:
+    """A button on a keyboard.
+
+    >>> f = lambda c: print(c, end='')  # The end='' argument avoids going to a new line
+    >>> k, e, y = Button('k', f), Button('e', f), Button('y', f)
+    >>> s = e.press().press().press()
+    eee
+    >>> caps = Button.caps_lock
+    >>> t = [x.press() for x in [k, e, y, caps, e, e, k, caps, e, y, e, caps, y, e, e]]
+    keyEEKeyeYEE
+    >>> u = Button('a', print).press().press().press()
+    A
+    A
+    A
+    """
+    caps_lock = CapsLock()
+
+    def __init__(self, letter, output):
+        assert letter in LOWERCASE_LETTERS
+        self.letter = letter
+        self.output = output
+        self.pressed = 0
+
+    def press(self):
+        """Call output on letter (maybe uppercased), then return the button that was pressed."""
+        if self.caps_lock.pressed % 2 == 1:
+            self.output(self.letter.upper())
+        else:
+            self.output(self.letter)
+        self.pressed += 1
+        return self
+
+class Keyboard:
+    """A keyboard.
+
+    >>> Button.caps_lock.pressed = 0  # Reset the caps_lock key
+    >>> bored = Keyboard()
+    >>> bored.type('hello')
+    >>> bored.typed
+    ['h', 'e', 'l', 'l', 'o']
+    >>> bored.keys['l'].pressed
+    2
+
+    >>> Button.caps_lock.press()
+    >>> bored.type('hello')
+    >>> bored.typed
+    ['h', 'e', 'l', 'l', 'o', 'H', 'E', 'L', 'L', 'O']
+    >>> bored.keys['l'].pressed
+    4
+    """
+    def __init__(self):
+        self.typed = []
+        self.keys = {letter : Button(letter, self.typed.append) for letter in LOWERCASE_LETTERS}  # Try a dictionary comprehension!
+
+    def type(self, word):
+        """Press the button for each letter in word."""
+        assert all([w in LOWERCASE_LETTERS for w in word]), 'word must be all lowercase'
+        for letter in word:
+            self.keys[letter].press()
+
+class Eye:
+    """An eye.
+
+    >>> Eye().draw()
+    '0'
+    >>> print(Eye(False).draw(), Eye(True).draw())
+    0 -
+    """
+    def __init__(self, closed=False):
+        self.closed = closed
+
+    def draw(self):
+        if self.closed:
+            return '-'
+        else:
+            return '•'
+
+class Bear:
+    """A bear.
+
+    >>> Bear().print()
+    ? 0o0?
+    """
+    def __init__(self):
+        self.nose_and_mouth = 'ᴥ'
+
+    def next_eye(self):
+        return Eye()
+
+    def print(self):
+        left, right = self.next_eye(), self.next_eye()
+        print('ʕ ' + left.draw() + self.nose_and_mouth + right.draw() + 'ʔ')
+
+class SleepyBear(Bear):
+    """A bear with closed eyes.
+
+    >>> SleepyBear().print()
+    ? -o-?
+    """
+    def next_eye(self):
+        return Eye(True)
+
+class WinkingBear(Bear):
+    """A bear whose left eye is different from its right eye.
+
+    >>> WinkingBear().print()
+    ? -o0?
+    """
+    def __init__(self):
+        self.count = 0
+        super().__init__()
+
+    def next_eye(self):
+        self.count += 1
+        return Eye (True) if self.count % 2 == 1 else Eye()
+
+
 def main():
-    for partition in sorted(partition_gen(6, 4)):
-        print(partition)
+
+    Button.caps_lock.pressed = 0
+    bored = Keyboard()
+    bored.type('hello')
+    print(bored.typed)
+    print(bored.keys['l'].pressed)
+    Button.caps_lock.press()
+    bored.type('hello')
+    print(bored.typed)
+    print(bored.keys['l'].pressed)
+    SleepyBear().print()
+    WinkingBear().print()
 
 if __name__ == "__main__":
     main()
